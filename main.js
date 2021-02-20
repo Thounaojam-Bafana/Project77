e5dc2cb87d048a8c09eab3f7e9ed5f09d116facd
Thounaojam-Bafana <76650032+Thounaojam-Bafana@users.noreply.github.com>
2021-02-20 15:30:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+first_name = "Vandame"
+last_name = "Thounaojam"
+
+full_name = first_name.concat( last_name);
+
+console.log(first_name);
+console.log(last_name);
+console.log(full_name);
+
+length_of_name = full_name.length;
+console.log(length_of_name);
+
+name1 = "Fruity Sinam";
+console.log(name1);
+
+replaced_name1 = name1.replace("Sinam", "Oinam");
+console.log(replaced_name1);
+
+name_in_caps = "ANANYA";
+name_in_small = "Shanaya";
+
+console.log(name_in_caps);
+console.log(name_in_small);
+
+making_lower_case = name_in_caps.toLowerCase();
+making_Upper_case = name_in_small.toUpperCase();
+
+console.log(making_Upper_case);
+console.log(making_lower_case);
+
+square_root = Math.sqrt(81);
+console.log(square_root);
+
+maximum_no = Math.max(52, 93, 18, 47);
+minimum_no = Math.min(77, 13, 85, 39);
+
+console.log(maximum_no);
+console.log(minimum_no);
+
+pow1 = Math.pow(5, 4);
+pow2 = Math.pow(8, 3);
+
+console.log(pow1);
+console.log(pow2);
+
+
